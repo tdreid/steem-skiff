d51a743117cbebaab2cdd642b1e881bd1d0e6f19
tdreid <trevor.d.reid@gmail.com>
2018-07-24 22:47:34 +0000

    Add login command

---
+++ steem-sak.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const current_version = require('./package.json').version;
+const sak = require('commander');
+
+sak    
+  .command('login <user> [pw]')
+  .description('Check if a user/key pair is legit')
+  .action(require('./login'));
+  
+sak
+    .version(current_version,'-v, --version')
+    .parse(process.argv);
