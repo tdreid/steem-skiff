dba9c3834d2176d68bae539c2ce68538919c31e8
tdreid <trevor.d.reid@gmail.com>
2018-07-25 00:22:33 +0000

    Fix command description and argument name

--- steem-sak.js
+++ steem-sak.js
@@ -4,8 +4,8 @@ const current_version = require('./package.json').version;
 const sak = require('commander');
 
 sak
-  .command('login <user> [pw]')
-  .description('Check if a user/key pair is legit')
+  .command('login <user> [password]')
+  .description('Check if a user/password pair is legit')
   .action(require('./login'));
 
 sak.version(current_version, '-v, --version').parse(process.argv);
